7761479df4facb6bd874396c10796add762cb856
ashwin153 <ashwin.madavan@gmail.com>
2015-02-22 05:45:35 -0600

    Made ssbl-android buildable

--- ssbl-android/src/main/java/com/hunnymustard/ssbl/android/activities/MainActivity.java
+++ ssbl-android/src/main/java/com/hunnymustard/ssbl/android/activities/MainActivity.java
@@ -1,7 +1,16 @@
 package com.hunnymustard.ssbl.android.activities;
 
 import android.app.Activity;
+import android.os.Bundle;
+
+import com.hunnymustard.ssbl.android.R;
 
 public class MainActivity extends Activity {
 	
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    
 }
