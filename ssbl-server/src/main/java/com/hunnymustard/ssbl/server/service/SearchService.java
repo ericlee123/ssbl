49595002c3f925efb81120bdeb621a0a1cb48ad9
ashwin153 <ashwin.madavan@gmail.com>
2015-03-14 13:59:04 -0700

    Fixed SearchService

--- ssbl-server/src/main/java/com/hunnymustard/ssbl/server/service/SearchService.java
+++ ssbl-server/src/main/java/com/hunnymustard/ssbl/server/service/SearchService.java
@@ -1,10 +1,17 @@
 package com.hunnymustard.ssbl.server.service;
 
+import java.util.List;
+
+import com.hunnymustard.ssbl.model.Event;
+import com.hunnymustard.ssbl.model.Location;
+import com.hunnymustard.ssbl.model.User;
+
 public interface SearchService {
 
-	List<User> getByProximity();
-	List<Event> getByProximity();
-	List<User> getByExample();
-	List<User> getByExample();
+	List<User> getUsersByProximity(Location current, Double radius);
+	List<User> getUsersByExample(User example);
+
+	List<Event> getEventsByProximity(Location current, Double radius);
+	List<Event> getEventsByExample(Event example);
 	
 }
--- ssbl-server/src/main/java/com/hunnymustard/ssbl/server/service/impl/SearchServiceHibernate.java
+++ ssbl-server/src/main/java/com/hunnymustard/ssbl/server/service/impl/SearchServiceHibernate.java
@@ -1,11 +1,47 @@
 package com.hunnymustard.ssbl.server.service.impl;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.hunnymustard.ssbl.model.Event;
+import com.hunnymustard.ssbl.model.Location;
+import com.hunnymustard.ssbl.model.User;
+import com.hunnymustard.ssbl.server.repository.EventRepository;
+import com.hunnymustard.ssbl.server.repository.UserRepository;
+import com.hunnymustard.ssbl.server.service.SearchService;
+
 @Service("searchService")
 @Transactional(propagation = Propagation.REQUIRED, readOnly=false)
 public class SearchServiceHibernate implements SearchService {
+
+	@Autowired
+	private UserRepository _userRepository;
+	
+	@Autowired
+	private EventRepository _eventRepository;
+	
+	@Override
+	public List<User> getUsersByProximity(Location current, Double radius) {
+		return _userRepository.findByProximity(current, radius);
+	}
+
+	@Override
+	public List<User> getUsersByExample(User example) {
+		return _userRepository.findByExample(example);
+	}
+
+	@Override
+	public List<Event> getEventsByProximity(Location current, Double radius) {
+		return _eventRepository.findByProximity(current, radius);
+	}
+
+	@Override
+	public List<Event> getEventsByExample(Event example) {
+		return _eventRepository.findByExample(example);
+	}
 
 }
