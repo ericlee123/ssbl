9d2f8440804a3c9d6fc8a74117f7c6243202cd97
ashwin153 <ashwin.madavan@gmail.com>
2015-03-15 22:15:55 -0700

    Fixed annotation error

--- ssbl-server/src/main/java/com/hunnymustard/ssbl/server/service/impl/EditServiceHibernate.java
+++ ssbl-server/src/main/java/com/hunnymustard/ssbl/server/service/impl/EditServiceHibernate.java
@@ -1,6 +1,9 @@
 package com.hunnymustard.ssbl.server.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.hunnymustard.ssbl.model.Event;
 import com.hunnymustard.ssbl.model.User;
@@ -8,6 +11,8 @@ import com.hunnymustard.ssbl.server.repository.EventRepository;
 import com.hunnymustard.ssbl.server.repository.UserRepository;
 import com.hunnymustard.ssbl.server.service.EditService;
 
+@Service("editService")
+@Transactional(propagation = Propagation.REQUIRED, readOnly=false)
 public class EditServiceHibernate implements EditService {
 
 	@Autowired
--- ssbl-server/src/main/java/com/hunnymustard/ssbl/server/service/impl/NotificationServiceHibernate.java
+++ ssbl-server/src/main/java/com/hunnymustard/ssbl/server/service/impl/NotificationServiceHibernate.java
@@ -3,12 +3,17 @@ package com.hunnymustard.ssbl.server.service.impl;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.hunnymustard.ssbl.model.Notification;
 import com.hunnymustard.ssbl.model.User;
 import com.hunnymustard.ssbl.server.repository.NotificationRepository;
 import com.hunnymustard.ssbl.server.service.NotificationService;
 
+@Service("notificationService")
+@Transactional(propagation = Propagation.REQUIRED, readOnly=false)
 public class NotificationServiceHibernate implements NotificationService {
 
 	@Autowired
